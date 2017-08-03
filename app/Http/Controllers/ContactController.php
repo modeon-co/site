f2ca21419046d7f52fdd97fc6fe43d752df34b5d
Caio Ladislau <caio@modeon.co>
2017-08-03 11:50:08 -0300

    Controller para envio de contato por email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,33 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Order;
-use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-  public function receipt(Request $in) {
-    $title = $in->demand;
-    if (empty($in->observation)) {
-      $in->observation = "Nenhuma";
-    }
-    $content = [
-      "name" => $in->name,
-      "email" => $in->email,
-      "phone" => $in->phone,
-      "demand" => $in->demand,
-      "observation" => $in->observation,
-    ];
-
-    Mail::send('emails.contact', ['title' => $title, 'content' => $content], function ($message) use($in)
+    public function receipt(Request $in)
     {
-      $message->subject("[Contato pelo site]");
-      $message->from('modeon.co@gmail.com', 'Modeon');
-      $message->to($in->email);
-      $message->cc('modeon.co@gmail.com');
-    });
-    return response()->json(['status' => 1, 'message' => 'Request completed']);
-  }
+        $title = $in->demand;
+        if (empty($in->observation)) {
+            $in->observation = "Nenhuma";
+        }
+        $content = [
+            "name" => $in->name,
+            "email" => $in->email,
+            "phone" => $in->phone,
+            "demand" => $in->demand,
+            "observation" => $in->observation,
+        ];
+
+        Mail::send('emails.contact', ['title' => $title, 'content' => $content], function ($message) use ($in) {
+            $message->subject("[Contato pelo site]");
+            $message->from('modeon.co@gmail.com', 'Modeon');
+            $message->to($in->email);
+            $message->cc('modeon.co@gmail.com');
+        });
+        return response()->json(['status' => 1, 'message' => 'Request completed']);
+    }
 }
