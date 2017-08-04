844093ef40aabb3fa42105c570637210b269ebe9
Caio Ladislau <caio@modeon.co>
2017-08-04 20:09:34 -0300

    Adiciona rotas para carregar portfólios

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 Route::any('contact/receipt', 'ContactController@receipt');
 
+Route::get('portfolio/{name?}', function ($name = null) {
+    $pages = ['klicia', 'sublimefts', 'comodoro', 'barbizan', 'vivaleve', 'feiradenoivas'];
+    if (in_array($name, $pages)) {
+        return view('portfolio.' . $name);
+    } else {
+        return redirect('/');
+    }
+});
+
 Route::get('/', function () {
     return view('main');
 });
